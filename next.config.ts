ab5c0ad428e48537324c6b544145d34cc818d650
[Ashish Ranjan Das] <[ashishrjd.2812@gmail.com]>
2025-07-10 14:40:37 +0530

    css config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // ✅ Required for static HTML export in Next.js 13+ App Router
-  reactStrictMode: true, // ✅ Optional but recommended
-  swcMinify: true,       // ✅ Optional for better performance
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
